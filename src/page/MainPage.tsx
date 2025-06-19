b2322cf0c03327da3b86d8268d606e6fc0354957
KimEunHye <gpqjr00@naver.com>
2025-06-20 04:05:42 +0900

    Feat: 실시간 데이터 모니터링 mock api 테스트

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -2,7 +2,22 @@ import Background from '@/components/UI/Background';
 import Header from '@/components/UI/Header';
 import Main from '@/components/UI/Main';
 
+import { useQuery } from '@tanstack/react-query';
+import { getLatestSensorData, getDataHistory } from '@/apis/SensorAxios';
+
 const MainPages = () => {
+  const { data: SensorData } = useQuery({
+    queryKey: ['latestSensorData'],
+    queryFn: () => getLatestSensorData({ zoneId: 1 }),
+  });
+
+  const { data: HistoryData } = useQuery({
+    queryKey: ['dataHistory'],
+    queryFn: () => getDataHistory({ zoneId: 1 }),
+  });
+
+  console.log('res', SensorData, HistoryData);
+
   return (
     <Background>
       <Header />
