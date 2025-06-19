2f1738119f424dbadd95473453a0e5e47493cc4e
KimEunHye <gpqjr00@naver.com>
2025-06-20 04:04:23 +0900

    Feat: query client 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MainPages from './page/MainPage';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <MainPages />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainPages />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
