0341ae30f9475fbd6d5e3c62b305aa367968dea6
KimEunHye <gpqjr00@naver.com>
2025-06-20 18:21:18 +0900

    Comment: 주석 위치 변경

--- src/apis/SensorAxios.ts
+++ src/apis/SensorAxios.ts
@@ -1,9 +1,9 @@
-import AxiosInstance from './axiosInstance';
+import AxiosInstance from './AxiosInstance';
 import type { SensorDataProps } from '@/types/type';
 
+// 최신 상태 조회
 export const getLatestSensorData = async ({ zoneId }: SensorDataProps) => {
   try {
-    // 최신 상태 조회
     const { data } = await AxiosInstance.get(`/zones/${zoneId}/latest`);
     return data;
   } catch (error) {
@@ -11,9 +11,9 @@ export const getLatestSensorData = async ({ zoneId }: SensorDataProps) => {
   }
 };
 
+// 과거 이력 조회
 export const getDataHistory = async ({ zoneId }: SensorDataProps) => {
   try {
-    // 과거 이력 조회
     const res = await AxiosInstance.get(`/zones/${zoneId}/history`);
     return res.data;
   } catch (error) {
