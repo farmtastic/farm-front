9ac07a61a4379dde8c3b88778298923ae5eb6c73
KimEunHye <gpqjr00@naver.com>
2025-06-20 18:20:35 +0900

    Feat: 알림 목록, 제어 이력 조회 api 함수 작성

--- src/apis/HistoryAxios.ts
+++ src/apis/HistoryAxios.ts
@@ -1,3 +1,21 @@
-const HistoryAxios = () => {};
+import AxiosInstance from './AxiosInstance';
 
-export default HistoryAxios;
+// 알림 목록 조회
+export const getNotifications = async () => {
+  try {
+    const { data } = await AxiosInstance.get('notifications');
+    return data;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
+
+// 제어 이력 조회
+export const getLogs = async () => {
+  try {
+    const { data } = await AxiosInstance.get('/controls/logs');
+    return data;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
