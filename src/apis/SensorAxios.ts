1333dc3d2432b66ab1b34d784ad0640d018fa01e
KimEunHye <gpqjr00@naver.com>
2025-07-01 03:17:48 +0900

    Rename: 파일 이름 수정

--- src/apis/SensorAxios.ts
+++ src/apis/SensorAxios.ts
@@ -1,8 +1,8 @@
 import AxiosInstance from './AxiosInstance';
-import type { SensorDataProps } from '@/types/type';
+import type { SensorDataAPIProps } from '@/types/type';
 
 // 최신 상태 조회
-export const getLatestSensorData = async ({ zoneId }: SensorDataProps) => {
+export const getLatestSensorData = async ({ zoneId }: SensorDataAPIProps) => {
   try {
     const { data } = await AxiosInstance.get(`/zones/${zoneId}/latest`);
     return data;
@@ -12,7 +12,7 @@ export const getLatestSensorData = async ({ zoneId }: SensorDataProps) => {
 };
 
 // 과거 이력 조회
-export const getDataHistory = async ({ zoneId }: SensorDataProps) => {
+export const getDataHistory = async ({ zoneId }: SensorDataAPIProps) => {
   try {
     const res = await AxiosInstance.get(`/zones/${zoneId}/history`);
     return res.data;
