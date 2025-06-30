755ce48d53adc12148da7a4e7d297ad5afa6fed7
KimEunHye <gpqjr00@naver.com>
2025-06-30 14:23:27 +0900

    Feat: 데이터 업데이트 컴포넌트 작성

---
+++ src/components/SensorData/LatestDataUpdate.tsx
@@ -0,0 +1,15 @@
+import UpdateIcon from '@/components/Icon/update.svg?react';
+
+const LatestDataUpdate = () => {
+  const time = '2025/07/03 20:38';
+  return (
+    <div className="text-xl flex items-center">
+      <span>최신 업데이트: {time}</span>
+      <button className="w-5 bg-transparent mx-2">
+        <UpdateIcon className="w-5 h-5" />
+      </button>
+    </div>
+  );
+};
+
+export default LatestDataUpdate;
