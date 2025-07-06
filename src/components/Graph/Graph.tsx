161e3b62718459e4e838b33a2cddef4da9351500
KimEunHye <gpqjr00@naver.com>
2025-07-07 01:45:40 +0900

    Feat: 그래프 컴포넌트 작성

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,5 +1,30 @@
+import ArticleTitle from '../UI/ArticleTitle';
+import { LineChart } from 'recharts';
+
 const Graph = () => {
-  return;
+  return (
+    <article className="p-45px rounded-contentsCard bg-ContentsColor">
+      <ArticleTitle>수치 그래프</ArticleTitle>
+      <div className="text-3xl mb-graphB mt-graphT">수위</div>
+      <LineChart
+        width={650}
+        height={350}
+        className="bg-BackgroundColor rounded-card outline-none"
+      ></LineChart>
+      <div className="text-3xl mb-graphB mt-graphT">조도</div>
+      <LineChart
+        width={650}
+        height={350}
+        className="bg-BackgroundColor rounded-card outline-none"
+      ></LineChart>
+      <div className="text-3xl mb-graphB mt-graphT">pH</div>
+      <LineChart
+        width={650}
+        height={350}
+        className="bg-BackgroundColor rounded-card outline-none"
+      ></LineChart>
+    </article>
+  );
 };
 
 export default Graph;
