6f7d259bef440392e70bdd4ec69ec3ef935c73dc
KimEunHye <gpqjr00@naver.com>
2025-07-07 01:47:06 +0900

    Design: css 수정 및 각 그래프 위치 맞춤

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -4,17 +4,27 @@ import SettingButton from './SettingButton';
 
 const Controls = () => {
   return (
-    <article className="m-contentsCard">
+    <article className="bg-ContentsColor p-45px rounded-contentsCard">
       <ArticleTitle>제어</ArticleTitle>
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col">
         <Card type="controls">
-          <div className="flex">
-            <div>수위제어</div>
+          <div className="m-16 flex justify-between flex-1">
+            <div className="text-4xl">수위 제어</div>
             <SettingButton />
           </div>
         </Card>
-        <Card type="controls">조도제어</Card>
-        <Card type="controls">pH제어</Card>
+        <Card type="controls">
+          <div className="m-16 flex justify-between flex-1">
+            <div className="text-4xl">조도 제어</div>
+            <SettingButton />
+          </div>
+        </Card>
+        <Card type="controls">
+          <div className="m-16 flex justify-between flex-1">
+            <div className="text-4xl">pH 제어</div>
+            <SettingButton />
+          </div>
+        </Card>
       </div>
     </article>
   );
