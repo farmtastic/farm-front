b17aa92441b7b0f0b9e0e0749956d4e3345df468
KimEunHye <gpqjr00@naver.com>
2025-07-07 14:52:35 +0900

    Fix: 모든 네트워크에 접속가능하도록 server 객체 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접속 허용
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
