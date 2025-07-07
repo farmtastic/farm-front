5c85db42cd8248c2ebf8e394a4cb0c1c78838a1c
KimEunHye <gpqjr00@naver.com>
2025-07-07 14:37:42 +0900

    Feat: 그래프 영역에서 width가 100%로 설정되도록 컴포넌트 추가

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,28 +1,22 @@
 import ArticleTitle from '../UI/ArticleTitle';
-import { LineChart } from 'recharts';
+import { LineChart, ResponsiveContainer } from 'recharts';
 
 const Graph = () => {
   return (
     <article className="p-45px rounded-contentsCard bg-ContentsColor">
       <ArticleTitle>수치 그래프</ArticleTitle>
       <div className="text-3xl mb-graphB mt-graphT">수위</div>
-      <LineChart
-        width={650}
-        height={350}
-        className="bg-BackgroundColor rounded-card outline-none"
-      ></LineChart>
+      <ResponsiveContainer width="100%" height={350}>
+        <LineChart className="bg-BackgroundColor rounded-card outline-none"></LineChart>
+      </ResponsiveContainer>
       <div className="text-3xl mb-graphB mt-graphT">조도</div>
-      <LineChart
-        width={650}
-        height={350}
-        className="bg-BackgroundColor rounded-card outline-none"
-      ></LineChart>
+      <ResponsiveContainer width="100%" height={350}>
+        <LineChart className="bg-BackgroundColor rounded-card outline-none"></LineChart>
+      </ResponsiveContainer>
       <div className="text-3xl mb-graphB mt-graphT">pH</div>
-      <LineChart
-        width={650}
-        height={350}
-        className="bg-BackgroundColor rounded-card outline-none"
-      ></LineChart>
+      <ResponsiveContainer width="100%" height={350}>
+        <LineChart className="bg-BackgroundColor rounded-card outline-none"></LineChart>
+      </ResponsiveContainer>
     </article>
   );
 };
