a3cd42d115d7862d4e1bd46cf6512cd934cc40bc
KimEunHye <gpqjr00@naver.com>
2025-07-07 19:41:00 +0900

    Feat: 모달창 닫는 함수 작성 및 모달 컴포넌트 추가

--- src/components/Controls/SettingButton.tsx
+++ src/components/Controls/SettingButton.tsx
@@ -1,10 +1,37 @@
-const SettingButton = () => {
+import type { STButtonType } from '@/types/type';
+import Modal from '../UI/Modal';
+import { useState } from 'react';
+
+const SettingButton = ({ id, type, data, onDelete, onSave }: STButtonType) => {
+  const [showModal, setShowModal] = useState(false);
+
   // 기록 조회 버튼이랑 스타일이 같고, 용도도 같음. 공통 컴포넌트로 빼기(크기, 배경색만 다름)
   // 버튼 클릭 시 모달창 렌더링
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <button className="rounded-3xl w-20 h-10 bg-ContentsColor px-4 py-1 text-xl transition hover:bg-slate-700/60">
-      설정
-    </button>
+    <div>
+      <button
+        onClick={() => setShowModal(true)}
+        className="rounded-3xl w-20 h-10 bg-ContentsColor px-4 py-1 text-xl transition hover:bg-slate-700/60 outline-none"
+      >
+        설정
+      </button>
+      {showModal && (
+        <Modal
+          id={id}
+          onSave={onSave}
+          onDelete={onDelete}
+          data={data}
+          type={type}
+          isOpen={showModal}
+          closeModal={closeModal}
+        />
+      )}
+    </div>
   );
 };
 
