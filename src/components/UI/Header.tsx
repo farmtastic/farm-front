51a1299e96fcf5e0d73ab8076374e3aa2c505127
KimEunHye <gpqjr00@naver.com>
2025-07-07 19:47:40 +0900

    Design: 헤더부분으로 데이터 최신 업데이트 컴포넌트 이동 및 폰트 크기 수정

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,9 +1,11 @@
 import { FaRegBell } from 'react-icons/fa';
+import LatestDataUpdate from '../SensorData/LatestDataUpdate';
 
 const Header = () => {
   return (
-    <header className="h-header flex justify-between items-center px-14">
-      <div className="text-header text-white">Parmtastic</div>
+    <header className="h-header flex justify-between items-center px-14 text-white">
+      <div className="text-header">Parmtastic</div>
+      <LatestDataUpdate />
       <FaRegBell size={40} />
     </header>
   );
