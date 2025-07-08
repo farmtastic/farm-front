ed6126e8df7d05db44e1e9b44f92eeca0ec72a1f
KimEunHye <gpqjr00@naver.com>
2025-07-08 13:39:43 +0900

    Feat: Drawer 표시/숨김 state 및 함수 추가

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { FaRegBell } from 'react-icons/fa';
 import LatestDataUpdate from '../SensorData/LatestDataUpdate';
+import Drawer from './Drawer';
 
 const Header = () => {
+  const [showDrawer, setShowDrawer] = useState<boolean>(false);
+
+  const onShowDrawer = () => {
+    setShowDrawer(true);
+  };
+
+  const onCloseDrawer = () => {
+    setShowDrawer(false);
+  };
+
   return (
     <header className="h-header flex justify-between items-center px-14 text-white">
-      <div className="text-header">Parmtastic</div>
+      <div className="text-header">Farmtastic</div>
       <LatestDataUpdate />
-      <FaRegBell size={40} />
+      <FaRegBell onClick={onShowDrawer} size={40} />
+      {showDrawer && <Drawer showDrawer={showDrawer} onClose={onCloseDrawer} />}
     </header>
   );
 };
