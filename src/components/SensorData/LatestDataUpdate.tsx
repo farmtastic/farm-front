894280fc8b8897e8ecc5a3366e001c618ac39921
KimEunHye <gpqjr00@naver.com>
2025-07-12 00:53:55 +0900

    Feat: 최신 데이터를 통한 업데이트 시간 변환 및 타임스탬프 변환 유틸함수 작성

--- src/components/SensorData/LatestDataUpdate.tsx
+++ src/components/SensorData/LatestDataUpdate.tsx
@@ -1,12 +1,34 @@
 import UpdateIcon from '@/components/Icon/update.svg?react';
+import LoadingSpinner from '../UI/LoadingSpinner';
+
+import { useQuery } from '@tanstack/react-query';
+import { getLatestSensorData } from '@/apis/SensorAxios';
+import { formatTimestamp } from '@/utils/formatTimestamp';
 
 const LatestDataUpdate = () => {
-  const time = '2025/07/03 20:38';
+  const {
+    data: sensorData,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ['latestSensorData'],
+    queryFn: () => getLatestSensorData({ zoneId: 1 }),
+  });
+
   return (
     <div className="text-3xl flex items-center">
-      <span>최신 업데이트: {time}</span>
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && (
+        <span>
+          최신 업데이트:{' '}
+          {formatTimestamp(sensorData.latestValues.WATER_LEVEL.timestamp)}
+        </span>
+      )}
       <button className="w-5 bg-transparent mx-2">
-        <UpdateIcon className="w-8 h-8 hover:animate-spin-once" />
+        <UpdateIcon
+          onClick={() => refetch()}
+          className="w-8 h-8 hover:animate-spin-once"
+        />
       </button>
     </div>
   );
