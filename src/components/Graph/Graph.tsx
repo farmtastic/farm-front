0b6b9857c0b73548f083533a941c4ae5c2158632
KimEunHye <gpqjr00@naver.com>
2025-07-12 00:53:19 +0900

    Feat: 더미데이터 추가

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,7 +1,40 @@
 import ArticleTitle from '../UI/ArticleTitle';
 import { LineChart, ResponsiveContainer } from 'recharts';
+// import { useQuery } from '@tanstack/react-query';
+// import { getDataHistory } from '@/apis/SensorAxios';
+
+const DummyData = {
+  historyValues: {
+    ILLUMINANCE: [
+      { timestamp: '2025-06-26T12:55:36.071133', value: 11000 },
+      { timestamp: '2025-06-26T13:00:36.071133', value: 12000 },
+      { timestamp: '2025-06-26T13:05:36.071133', value: 12000 },
+    ],
+    PH: [
+      { timestamp: '2025-06-26T12:55:36.057906', value: 6.7 },
+      { timestamp: '2025-06-26T13:03:36.057906', value: 6.7 },
+      { timestamp: '2025-06-26T13:05:36.057906', value: 6.5 },
+    ],
+    WATER_LEVEL: [
+      { timestamp: '2025-06-26T12:55:36.064386', value: 90 },
+      { timestamp: '2025-06-26T13:00:36.064386', value: 87.5 },
+      { timestamp: '2025-06-26T13:05:36.064386', value: 85.2 },
+    ],
+  },
+  latestValues: null,
+  zoneId: 1,
+  zoneName: '스마트팜 A-1',
+};
 
 const Graph = () => {
+  // 과거 이력 조회 쿼리
+  // const { data: HistoryData } = useQuery({
+  //   queryKey: ['dataHistory'],
+  //   queryFn: () => getDataHistory({ zoneId: 1 }),
+  // });
+
+  // console.log(HistoryData);
+
   return (
     <article className="p-45px rounded-contentsCard bg-ContentsColor">
       <ArticleTitle>수치 그래프</ArticleTitle>
