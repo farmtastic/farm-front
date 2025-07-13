21da3f45e89b1f2cfe5601aa5a916acde7a7b0e9
KimEunHye <gpqjr00@naver.com>
2025-07-14 02:26:12 +0900

    Design: html 태그 변경 및 각 데이터에 id 추가

--- src/components/UI/Drawer.tsx
+++ src/components/UI/Drawer.tsx
@@ -5,14 +5,17 @@ import { formatKoreanTimestamp } from '@/utils/formatKoreanTimestamp';
 const CustomDrawer = ({ showDrawer, onClose }: DrawerProps) => {
   const data = [
     {
+      id: 1,
       timestamp: '2025-06-26T12:55:36.071133',
       contents: 'pH 저하로 펌프 ON',
     },
     {
+      id: 2,
       timestamp: '2025-06-28T12:05:36.071133',
       contents: '수위 저하로 펌프 ON',
     },
     {
+      id: 3,
       timestamp: '2025-06-30T01:25:36.071133',
       contents: '조도 저하로 조명 ON',
     },
@@ -25,20 +28,31 @@ const CustomDrawer = ({ showDrawer, onClose }: DrawerProps) => {
       anchor="right"
       slotProps={{
         paper: {
-          className: 'bg-ContentsColor w-drawer',
+          className: 'w-drawer !bg-ContentsColor',
         },
       }}
     >
-      <div className="flex justify-between text-4xl p-7">
+      <div className="flex justify-between text-4xl p-7 text-white">
         <div>알림 내역</div>
-        <div>X</div>
-      </div>
-      {data.map((item) => (
-        <div className="flex flex-col justify-center p-7 h-drawerList text-3xl">
-          <div>{formatKoreanTimestamp(item.timestamp)}</div>
-          <div>{item.contents}</div>
+        <div onClick={onClose} className="cursor-pointer">
+          X
         </div>
-      ))}
+      </div>
+      <ul className="divide-y divide-gray-800">
+        {data.map((item) => (
+          <li
+            key={item.id}
+            className="px-7 py-4 flex flex-col gap-1 hover:bg-BackgroundColor transition"
+          >
+            <span className="text-2xl text-gray-400">
+              {formatKoreanTimestamp(item.timestamp)}
+            </span>
+            <span className="text-3xl font-medium text-white">
+              {item.contents}
+            </span>
+          </li>
+        ))}
+      </ul>
     </Drawer>
   );
 };
