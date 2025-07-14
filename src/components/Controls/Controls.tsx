df4e870076387b6b0d0cdaf049f7ae82c269a4fb
KimEunHye <gpqjr00@naver.com>
2025-07-15 02:34:21 +0900

    Feat: 생성, 수정, 제거 mutation 적용

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,6 +1,6 @@
 import ArticleTitle from '../UI/ArticleTitle';
 import Card from '../UI/Card';
-import SettingButton from './SettingButton';
+import Button from './Button';
 import type { RuleData, RulesProps } from '@/types/type';
 
 import { useQuery, useMutation } from '@tanstack/react-query';
@@ -43,30 +43,34 @@ const Controls = () => {
     },
   ];
 
-  const water = rulesData.filter((item) => item.ruleName === 'water');
-  const illuminance = rulesData.filter(
-    (item) => item.ruleName === 'illuminance'
-  );
-  const pH = rulesData.filter((item) => item.ruleName === 'pH');
-  console.log('임계값 조회', water, illuminance, pH);
-
-  // 업데이트 데이터
-  const updateData = {
-    threshold: 150,
-  };
-
   //규칙 조회 쿼리
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ['rulesData'],
     queryFn: () => getRules(),
   });
+
   // console.log(data);
+  const water =
+    data === undefined
+      ? ''
+      : data.filter((item: RuleData) => item.ruleName === 'water');
+  const illuminance =
+    data === undefined
+      ? ''
+      : data.filter((item: RuleData) => item.ruleName === 'illuminance');
+  const pH =
+    data === undefined
+      ? ''
+      : data.filter((item: RuleData) => item.ruleName === 'pH');
+  // console.log('임계값 조회', water, illuminance, pH);
+
   // 규칙 생성 mutation
   const createMutation = useMutation({
     mutationFn: (newData: RuleData) => createRules({ ...newData }),
     onSuccess: (data) => {
       console.log('생성 성공:', data);
       alert(data.message);
+      refetch();
     },
     onError: (error) => {
       console.error('생성 실패:', error);
@@ -80,7 +84,12 @@ const Controls = () => {
       updateRules({ ruleId, newData }),
     onSuccess: (data) => {
       console.log('수정 성공:', data);
-      alert(data.message);
+      alert(
+        data.message === undefined
+          ? '수정이 완료되었습니다.'
+          : '수정에 실패하였습니다.'
+      );
+      refetch();
     },
     onError: (error) => {
       console.error('수정 실패:', error);
@@ -100,71 +109,81 @@ const Controls = () => {
       alert('규칙 삭제 실패');
     },
   });
+  // 수정이 완료되면 센서데이터 카드도 새로고침하기
+  const onSave = (data: number, id: number, type: string) => {
+    const unit =
+      type === 'water' ? water : type === 'illuminance' ? illuminance : pH;
+    if (unit.length !== 0) {
+      console.log('수정', type, unit.length);
+      const newData = {
+        ...water[0],
+        threshold: data,
+      };
+      updateMutation.mutate({ ruleId: id, newData });
+    } else {
+      console.log(data, id, type, '생성');
+      const newCreateData = {
+        ruleName: type,
+        sensorId: id,
+        conditionOp: '테스트op',
+        threshold: data,
+        actuatorId: type === 'water' ? 7 : type === 'illuminance' ? 8 : 9,
+        command: '테스트cd',
+        active: true,
+      };
+      createMutation.mutate(newCreateData);
+    }
+  };
 
-  // const onSave = (data, id) => {
-  //   if (water.length !== 0) { 실제 데이터로 조건 수정 필요
-  //     const newData = {
-  //       ...water,
-  //       ...updateData,
-  //     };
-  //     updateMutation.mutate({ ruleId: id, newData });
-  //   } else {
-  //     createMutation.mutate(newCreateData);
-  //   }
-  // };
+  const onDelete = (id: number) => deleteMutation.mutate({ ruleId: id });
 
-  // const onDelete={(id) => () => deleteMutation.mutate({ ruleId: id})}
+  const controlConfigs = [
+    {
+      label: '수위 제어',
+      type: 'water',
+      id: 1,
+      data: water,
+      onSave: (data: number, id: number, type: string) =>
+        onSave(data, id, type),
+      onDelete: (id: number) => onDelete(id),
+    },
+    {
+      label: '조도 제어',
+      type: 'illuminance',
+      id: 2,
+      data: illuminance,
+      onSave: (data: number, id: number, type: string) =>
+        onSave(data, id, type),
+    },
+    {
+      label: 'pH 제어',
+      type: 'PH',
+      id: 3,
+      data: rulesData.filter((item) => item.ruleName === 'pH'),
+      onSave: (data: number, id: number, type: string) =>
+        onSave(data, id, type),
+    },
+  ];
 
   return (
     <article className="bg-ContentsColor p-45px rounded-contentsCard">
       <ArticleTitle>제어</ArticleTitle>
       <div className="flex flex-col">
-        <Card type="controls">
-          <div className="m-16 flex justify-between flex-1">
-            <div className="text-4xl">수위 제어</div>
-            <SettingButton
-              id={1}
-              onSave={() => {
-                if (water.length !== 0) console.log('update');
-                else console.log('create');
-              }}
-              onDelete={(id) => console.log('delete id', id)}
-              type="water"
-              data={water}
-            />
-          </div>
-        </Card>
-        <Card type="controls">
-          <div className="m-16 flex justify-between flex-1">
-            <div className="text-4xl">조도 제어</div>
-            <SettingButton
-              id={2}
-              onSave={() => {
-                if (illuminance.length !== 0) console.log('update');
-                else console.log('create');
-              }}
-              onDelete={(id) => console.log('delete id', id)}
-              type="illuminance"
-              data={illuminance}
-            />
-          </div>
-        </Card>
-        <Card type="controls">
-          <div className="m-16 flex justify-between flex-1">
-            <div className="text-4xl">pH 제어</div>
-            <SettingButton
-              id={3}
-              onSave={(saveData: number, id: number) => {
-                if (pH.length !== 0) console.log('update', saveData, id);
-                else console.log('create', saveData);
-              }}
-              onDelete={(id) => console.log('delete id', id)}
-              type="PH"
-              // create, update 테스트를 위해 따로 필터링
-              data={rulesData.filter((item) => item.ruleName === 'pH')}
-            />
-          </div>
-        </Card>
+        {controlConfigs.map((cfg) => (
+          <Card type="controls" key={cfg.id}>
+            <div className="m-16 flex justify-between flex-1">
+              <div className="text-4xl">{cfg.label}</div>
+              <Button
+                btnType="controls"
+                id={cfg.id}
+                onSave={cfg.onSave}
+                onDelete={(id) => deleteMutation.mutate({ ruleId: id })}
+                type={cfg.type}
+                data={cfg.data}
+              />
+            </div>
+          </Card>
+        ))}
       </div>
     </article>
   );
