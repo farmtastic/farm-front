0229affc4f53a039427b19046239136df209d93f
KimEunHye <gpqjr00@naver.com>
2025-07-15 02:36:49 +0900

    Feat: 실제 데이터 적용

--- src/components/SensorData/SensorData.tsx
+++ src/components/SensorData/SensorData.tsx
@@ -6,30 +6,25 @@ import LoadingSpinner from '../UI/LoadingSpinner';
 import { useQuery } from '@tanstack/react-query';
 import { getLatestSensorData, getDataHistory } from '@/apis/SensorAxios';
 
-// 임시 데이터
-const waterHistory = 4.8;
-const sunHistory = 590;
-const pHHistory = 4.9;
-
-const dataDummyData = {
-  historyValues: null,
-  latestValues: {
-    ILLUMINANCE: {
-      timestamp: '2025-06-26T12:55:36.071133',
-      value: 12000,
-    },
-    PH: {
-      timestamp: '2025-06-26T12:55:36.057906',
-      value: 6.5,
-    },
-    WATER_LEVEL: {
-      timestamp: '2025-06-26T12:55:36.064386',
-      value: 85.2,
-    },
-  },
-  zoneId: 1,
-  zoneName: '스마트팜 A-1',
-};
+// const DummyData = {
+//   historyValues: null,
+//   latestValues: {
+//     ILLUMINANCE: {
+//       timestamp: '2025-06-26T12:55:36.071133',
+//       value: 12000,
+//     },
+//     PH: {
+//       timestamp: '2025-06-26T12:55:36.057906',
+//       value: 6.5,
+//     },
+//     WATER_LEVEL: {
+//       timestamp: '2025-06-26T12:55:36.064386',
+//       value: 85.2,
+//     },
+//   },
+//   zoneId: 1,
+//   zoneName: '스마트팜 A-1',
+// };
 
 const SensorData = () => {
   // 최신 상태 조회 쿼리
@@ -49,7 +44,19 @@ const SensorData = () => {
     queryFn: () => getDataHistory({ zoneId: 1 }),
   });
 
-  // console.log(sensorData, isFetching, isLoading, HistoryData);
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  // 가장 최근의 history 데이터가 배열 맨뒤에 추가된다고 가정함
+  const waterHistory =
+    HistoryData.historyValues.WATER_LEVEL[
+      HistoryData.historyValues.WATER_LEVEL.length - 1
+    ].value;
+  const sunHistory = HistoryData.historyValues.LIGHT[0].value; // UI 테스트를 위해 배열 인덱스 임의 지정
+  const pHHistory = HistoryData.historyValues.PH[0].value; // UI 테스트를 위해 배열 인덱스 임의 지정
+
+  // console.log(sensorData, 'sensor', isLoading, HistoryData);
 
   // 실시간 수치들을 넘겨주면서 과거 이력 조회api의 가장 최신 데이터도 같이 넘겨줌
   // 가장 최신 데이터를 이용해서 이전 수치에 비해 얼마나 증/감/유지 되었는지를 표시.
@@ -66,18 +73,18 @@ const SensorData = () => {
             <ThresholdCard
               type="water"
               // sensorData.latestValues.WATER_LEVEL.value
-              data={dataDummyData.latestValues.WATER_LEVEL.value}
+              data={sensorData.latestValues.WATER_LEVEL.value}
               history={waterHistory}
             />
           )}
         </Card>
         <Card type="sensors">
-          {isLoading && <LoadingSpinner />}
-          {!isLoading && (
+          {(isLoading || isFetching) && <LoadingSpinner />}
+          {(!isLoading || !isFetching) && (
             <ThresholdCard
               type="illuminance"
               // sensorData.latestValues.ILLUMINANCE.value
-              data={dataDummyData.latestValues.ILLUMINANCE.value}
+              data={sensorData.latestValues.LIGHT.value}
               history={sunHistory}
             />
           )}
@@ -88,7 +95,7 @@ const SensorData = () => {
             <ThresholdCard
               type="PH"
               // sensorData.latestValues.PH.value
-              data={dataDummyData.latestValues.PH.value}
+              data={sensorData.latestValues.PH.value}
               history={pHHistory}
             />
           )}
