07543059027c63592603fe35bade36f50a9f50aa
KimEunHye <gpqjr00@naver.com>
2025-07-18 03:51:56 +0900

    Refactor: 이력 조회 버튼으로도 사용 가능하도록 리팩토링 후 기능 추가

--- src/components/Controls/Button.tsx
+++ src/components/Controls/Button.tsx
@@ -1,8 +1,9 @@
-import type { STButtonType } from '@/types/type';
+import type { HistoryType, STButtonType, ControlsDataType } from '@/types/type';
 import Modal from '../UI/Modal';
+import HistoryModal from '../Graph/HistoryModal';
 import { useState } from 'react';
 
-const SettingButton = ({
+const Button = ({
   btnType,
   id,
   type,
@@ -12,29 +13,38 @@ const SettingButton = ({
 }: STButtonType) => {
   const [showModal, setShowModal] = useState(false);
 
-  // 기록 조회 버튼이랑 스타일이 같고, 용도도 같음. 공통 컴포넌트로 빼기(배경색, 폰트색만 다름)
-  // ButtonType을 인자로 받아서 타입이 controls일때와 history일 경우 두 가지로 버튼의 이름을 줌
-  // onSave와 onDelete는 옵셔널 체이닝을 사용해 history 모달의 경우 이 두가지 함수는 인자로 받지 않음.
-  // 버튼 클릭 시 모달창 렌더링
-
   const closeModal = () => {
     setShowModal(false);
+  };
+
+  const dynamicCss = {
+    controls:
+      'rounded-3xl w-20 h-10 bg-ContentsColor px-4 py-1 text-xl transition hover:bg-slate-700/60 outline-none',
+    history:
+      'rounded-3xl w-20 h-10 bg-BackgroundColor hover:bg-slate-700/60 outline-none transition text-sm mt-5',
   };
 
   return (
     <div>
       <button
         onClick={() => setShowModal(true)}
-        className="rounded-3xl w-20 h-10 bg-ContentsColor px-4 py-1 text-xl transition hover:bg-slate-700/60 outline-none"
+        className={`${dynamicCss[btnType]}`}
       >
         {btnType === 'controls' ? '설정' : '기록 조회'}
       </button>
-      {showModal && (
+      {showModal && btnType === 'history' && (
+        <HistoryModal
+          isOpen={showModal}
+          closeModal={closeModal}
+          data={data as HistoryType[]}
+        />
+      )}
+      {showModal && btnType === 'controls' && (
         <Modal
           id={id}
           onSave={onSave}
           onDelete={onDelete}
-          data={data}
+          data={data as ControlsDataType[]}
           type={type}
           isOpen={showModal}
           closeModal={closeModal}
@@ -44,4 +54,4 @@ const SettingButton = ({
   );
 };
 
-export default SettingButton;
+export default Button;
