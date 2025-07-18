39f90676ac7117b314b9b08378c1240d5c5c53a4
KimEunHye <gpqjr00@naver.com>
2025-07-19 01:36:57 +0900

    Feat: Background 컴포넌트 padding css 값 추가

--- src/components/UI/Background.tsx
+++ src/components/UI/Background.tsx
@@ -2,7 +2,7 @@ import type { Children } from '@/types/type';
 
 const Background = ({ children }: Children) => {
   return (
-    <div className="w-screen min-h-screen bg-BackgroundColor m-0 p-0 flex flex-col">
+    <div className="w-screen min-h-screen bg-BackgroundColor m-0 px-12 flex flex-col">
       {children}
     </div>
   );
