ab35ea427ebafc0524514fb2a4f9d9a0919ad535
KimEunHye <gpqjr00@naver.com>
2025-07-20 23:23:18 +0900

    Comment: 불필요한 주석 삭제

--- src/components/SensorData/SensorData.tsx
+++ src/components/SensorData/SensorData.tsx
@@ -6,37 +6,12 @@ import LoadingSpinner from '../UI/LoadingSpinner';
 import { useQuery } from '@tanstack/react-query';
 import { getLatestSensorData, getDataHistory } from '@/apis/SensorAxios';
 
-// const DummyData = {
-//   historyValues: null,
-//   latestValues: {
-//     ILLUMINANCE: {
-//       timestamp: '2025-06-26T12:55:36.071133',
-//       value: 12000,
-//     },
-//     PH: {
-//       timestamp: '2025-06-26T12:55:36.057906',
-//       value: 6.5,
-//     },
-//     WATER_LEVEL: {
-//       timestamp: '2025-06-26T12:55:36.064386',
-//       value: 85.2,
-//     },
-//   },
-//   zoneId: 1,
-//   zoneName: '스마트팜 A-1',
-// };
-
 const SensorData = () => {
   // 최신 상태 조회 쿼리
   // isLoading => 로딩 스피너, isFetching => 새로고침
 
-  // 회의때 물어볼것
-  // 수위는 임계값을 설정할 필요가 없는가?
-  // 규칙 조회 api에서 받아오는 수위 경고, 조명 자동 조절은 뭔가. 프론트에서 신경써야할 부분인가?
-  // api 호출할때 기존 방식과 달라진 점이 있는지? (필요한 데이터가 바꼈다던지 등등...)
-  // 그래프 이력 데이터는 3시간 이내의 데이터만 저장되는것인가?
-  // 알림 더미데이터 sql문 요청
-  // 그래프 이력 조회 데이터 기록 주기...?
+  // 센서 데이터 업데이트 주기 10초
+  // 그래프에 쓰이는 데이터는 3시간마다 줌.
 
   const {
     data: sensorData,
