9e722094506094862b5dffda51d8602595417f70
KimEunHye <gpqjr00@naver.com>
2025-07-21 18:42:36 +0900

    Design: 카드 css  수정

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -3,14 +3,14 @@ import type { CardProps } from '@/types/type';
 const Card = ({ children, type }: CardProps) => {
   // 동적 CSS를 위한 객체
   const cardDynamicCss = {
-    sensors: 'w-210 h-210 justify-center',
-    controls: 'w-full h-140',
-    graphs: 'w-210 h-210',
+    sensors: 'w-full h-48 justify-center',
+    controls: 'w-full h-36',
+    graphs: 'w-210 h-48',
   };
 
   return (
     <div
-      className={`${cardDynamicCss[type]} bg-BackgroundColor rounded-card mx-1 mt-card mb-9 flex items-center`}
+      className={`${cardDynamicCss[type]} bg-BackgroundColor rounded-card mx-1 flex items-center`}
     >
       {children}
     </div>
