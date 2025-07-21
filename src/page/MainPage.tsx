717b729b578878c43adfafad6c1ded944bfa2dc1
KimEunHye <gpqjr00@naver.com>
2025-07-21 22:47:45 +0900

    Refactor: 알림 목록 조회 쿼리 이동

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,18 +1,8 @@
 import Background from '@/components/UI/Background';
 import Header from '@/components/UI/Header';
 import Main from '@/components/UI/Main';
-import { useQuery } from '@tanstack/react-query';
-import { getNotifications } from '@/apis/HistoryAxios';
 
 const MainPages = () => {
-  // 알림 목록 조회 쿼리
-  const { data: NotiData } = useQuery({
-    queryKey: ['notification'],
-    queryFn: () => getNotifications(),
-  });
-
-  console.log(NotiData);
-
   return (
     <Background>
       <Header />
