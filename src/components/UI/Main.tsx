4a643c2a0bebb538412a9abad36eadeb4f8938be
KimEunHye <gpqjr00@naver.com>
2025-07-21 18:43:44 +0900

    Feat: 동적 마진 계산 hook 적용

--- src/components/UI/Main.tsx
+++ src/components/UI/Main.tsx
@@ -2,15 +2,30 @@ import SensorData from '../SensorData/SensorData';
 import Controls from '../Controls/Controls';
 import Graph from '../Graph/Graph';
 
+import { useRef } from 'react';
+import { useDynamicMargin } from '@/hooks/useDynamicMargin';
+
 const Main = () => {
+  const sensorRef = useRef<HTMLDivElement>(null);
+  const controlsRef = useRef<HTMLDivElement>(null);
+  const rightRef = useRef<HTMLDivElement>(null);
+
+  useDynamicMargin(sensorRef, controlsRef, rightRef);
+
   return (
-    <main className="flex pb-14 text-white">
-      <section className="mx-14 flex-1 min-w-0 min-h-article">
-        <SensorData />
-        <Controls />
+    <main className="box-border grid items-stretch grid-cols-2 gap-x-8 px-14 pb-14 text-white">
+      <section className="box-border flex flex-col min-h-full">
+        <div ref={sensorRef}>
+          <SensorData />
+        </div>
+        <div ref={controlsRef}>
+          <Controls />
+        </div>
       </section>
-      <section className="mr-14 flex-1 min-w-0 min-h-article">
-        <Graph />
+      <section className="box-border min-h-full space-y-6">
+        <div ref={rightRef}>
+          <Graph />
+        </div>
       </section>
     </main>
   );
