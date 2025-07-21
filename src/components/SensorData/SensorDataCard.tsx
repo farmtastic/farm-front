1902a6fef48c639d06a0290d3aa5bc9514dc70fc
KimEunHye <gpqjr00@naver.com>
2025-07-21 22:44:36 +0900

    Feat: 수위 경고 여부 전달 및 아이콘 크기 수정

--- src/components/SensorData/SensorDataCard.tsx
+++ src/components/SensorData/SensorDataCard.tsx
@@ -3,16 +3,26 @@ import Sun from '@/components/Icon/sun.svg?react';
 import WaterIcon from '@/components/Icon/water.svg?react';
 import type { SensorDataType } from '@/types/type';
 import DiffStatus from './DiffStatus';
+import WaterLow from './WaterLow';
 
-const SensorDataCard = ({ type, data, history }: SensorDataType) => {
+const SensorDataCard = ({
+  type,
+  data,
+  history,
+  isWaterLow = false,
+  isWaterHigher = false,
+}: SensorDataType) => {
   return (
-    <div className="flex items-center gap-2 justify-between w-full px-24">
+    <div className="relative flex items-center gap-2 justify-between w-full px-24">
+      {type === 'water' && (isWaterLow || isWaterHigher) && (
+        <WaterLow isWaterHigher={isWaterHigher} isWaterLow={isWaterLow} />
+      )}
       {type === 'water' ? (
-        <WaterIcon className="w-28 h-28 mb-4" />
+        <WaterIcon className="w-24 h-24 mb-4" />
       ) : type === 'illuminance' ? (
-        <Sun className="w-32 h-32" />
+        <Sun className="w-28 h-28" />
       ) : (
-        <PHIcon className="w-32 h-32" />
+        <PHIcon className="w-28 h-28" />
       )}
       <DiffStatus type={type} data={data} history={history} />
     </div>
