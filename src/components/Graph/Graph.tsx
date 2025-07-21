e42408ec0ea93ed2f4f77dd2885baa3bea067ecf
KimEunHye <gpqjr00@naver.com>
2025-07-21 18:44:07 +0900

    Refactor: 그래프 더미데이터 제거

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -6,29 +6,6 @@ import LoadingSpinner from '../UI/LoadingSpinner';
 import type { HistoryDataType } from '@/types/type';
 import GraphInfoTooltip from './GraphInfoTooltip';
 
-const DummyData = {
-  historyValues: {
-    ILLUMINANCE: [
-      { timestamp: '2025-06-26T12:55:36.071133', value: 11000 },
-      { timestamp: '2025-06-26T13:00:36.071133', value: 12000 },
-      { timestamp: '2025-06-26T13:05:36.071133', value: 12000 },
-    ],
-    PH: [
-      { timestamp: '2025-06-26T12:55:36.057906', value: 6.7 },
-      { timestamp: '2025-06-26T13:03:36.057906', value: 6.7 },
-      { timestamp: '2025-06-26T13:05:36.057906', value: 6.5 },
-    ],
-    WATER_LEVEL: [
-      { timestamp: '2025-06-26T12:55:36.064386', value: 90 },
-      { timestamp: '2025-06-26T13:00:36.064386', value: 87.5 },
-      { timestamp: '2025-06-26T13:05:36.064386', value: 85.2 },
-    ],
-  },
-  latestValues: null,
-  zoneId: 1,
-  zoneName: '스마트팜 A-1',
-};
-
 const Graph = () => {
   // 과거 이력 조회 쿼리
   const { data: HistoryData, isLoading } = useQuery({
@@ -54,11 +31,13 @@ const Graph = () => {
       threshold: d.threshold === null ? 0 : d.threshold,
     })
   );
-  const pHHistory = DummyData.historyValues.PH.map((d) => ({
-    ...d,
-    type: 'PH' as const,
-    threshold: 5.5,
-  }));
+  const pHHistory = HistoryData.historyValues?.PH?.map(
+    (d: HistoryDataType) => ({
+      ...d,
+      type: 'PH' as const,
+      threshold: d.threshold === null ? 0 : d.threshold,
+    })
+  );
 
   return (
     <article className="p-45px rounded-contentsCard bg-ContentsColor">
