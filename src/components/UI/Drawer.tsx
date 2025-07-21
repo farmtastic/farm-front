6d64b0762458e592b6fe50eec2b3bb797cdf37a5
KimEunHye <gpqjr00@naver.com>
2025-07-21 22:45:40 +0900

    Refactor: api 데이터 적용

--- src/components/UI/Drawer.tsx
+++ src/components/UI/Drawer.tsx
@@ -2,25 +2,7 @@ import Drawer from '@mui/material/Drawer';
 import type { DrawerProps } from '@/types/type';
 import { formatKoreanTimestamp } from '@/utils/formatKoreanTimestamp';
 
-const CustomDrawer = ({ showDrawer, onClose }: DrawerProps) => {
-  const data = [
-    {
-      id: 1,
-      timestamp: '2025-06-26T12:55:36.071133',
-      contents: 'pH 저하로 펌프 ON',
-    },
-    {
-      id: 2,
-      timestamp: '2025-06-28T12:05:36.071133',
-      contents: '수위 저하로 펌프 ON',
-    },
-    {
-      id: 3,
-      timestamp: '2025-06-30T01:25:36.071133',
-      contents: '조도 저하로 조명 ON',
-    },
-  ];
-
+const CustomDrawer = ({ showDrawer, onClose, data }: DrawerProps) => {
   return (
     <Drawer
       open={showDrawer}
@@ -41,14 +23,14 @@ const CustomDrawer = ({ showDrawer, onClose }: DrawerProps) => {
       <ul className="divide-y divide-gray-800">
         {data.map((item) => (
           <li
-            key={item.id}
+            key={item.createdAt}
             className="px-7 py-4 flex flex-col gap-1 hover:bg-BackgroundColor transition"
           >
             <span className="text-2xl text-gray-400">
-              {formatKoreanTimestamp(item.timestamp)}
+              {formatKoreanTimestamp(item.createdAt)}
             </span>
             <span className="text-3xl font-medium text-white">
-              {item.contents}
+              {item.message}
             </span>
           </li>
         ))}
