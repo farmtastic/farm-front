9310f22873f2ad1ea2a4f2bd3d053a0b83e88180
KimEunHye <gpqjr00@naver.com>
2025-07-24 14:04:15 +0900

    Feat: 툴팁에 초도 렌더링 되도록 추가

--- src/components/Graph/CustomWaterTooltip.tsx
+++ src/components/Graph/CustomWaterTooltip.tsx
@@ -4,10 +4,9 @@ import { formatStatus } from '@/utils/formatStatus';
 
 const CustomWaterTooltip = ({ active, payload }: CustomTooltipProps) => {
   if (!active || !payload?.length) return null;
-
   const { timestamp, status } = payload[0].payload;
 
-  const date = dayjs(timestamp).format('MM.DD HH:mm');
+  const date = dayjs(timestamp).format('MM.DD HH:mm:ss');
   return (
     <div className="bg-white/90 text-black rounded-lg px-3 py-2 shadow border border-gray-300 text-xs flex flex-col items-center">
       <div style={{ marginBottom: 4 }}>{date}</div>
