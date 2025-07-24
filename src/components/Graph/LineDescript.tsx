5dde117b0e3fa03e3345cabe8c22fa5790056ad4
KimEunHye <gpqjr00@naver.com>
2025-07-24 17:19:25 +0900

    Refactor: 수위 그래프의 경우 임계값 선이 나타나지 않도록 수정

--- src/components/Graph/LineDescript.tsx
+++ src/components/Graph/LineDescript.tsx
@@ -1,10 +1,12 @@
-const LineDescript = () => {
+const LineDescript = ({ type }: { type: 'water' | 'none-water' }) => {
   return (
     <div className="absolute left-16 bottom-2 p-1 flex items-center gap-6 text-xs ">
-      <div className="flex items-center gap-1">
-        <span className="inline-block w-8 h-1 rounded bg-[#ff6881]" />
-        <span className="text-gray-200">임계값</span>
-      </div>
+      {type === 'none-water' && (
+        <div className="flex items-center gap-1">
+          <span className="inline-block w-8 h-1 rounded bg-[#ff6881]" />
+          <span className="text-gray-200">임계값</span>
+        </div>
+      )}
       <div className="flex items-center gap-1">
         <span className="inline-block w-8 h-1 rounded bg-[#58a6ff]" />
         <span className="text-gray-200">수위</span>
